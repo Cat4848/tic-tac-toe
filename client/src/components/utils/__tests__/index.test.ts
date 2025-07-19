882d6469271b77a7f03cc13f992ebfbbd161f022
Cat4848 <catalinrol@gmail.com>
2025-07-19 11:38:56 +0100

    test: more on the winning row side

--- client/src/components/utils/__tests__/index.test.ts
+++ client/src/components/utils/__tests__/index.test.ts
@@ -7,6 +7,12 @@ test("if it validates correctly a winning row", () => {
   expect(isWinner).toBe(true);
 });
 
+test("if it validates correctly a winning row", () => {
+  const validValues: SquareValue[] = ["X", "X", "X", "X"];
+  const isWinner = isRowWinner(validValues);
+  expect(isWinner).toBe(true);
+});
+
 test("if it validates correctly a losing row", () => {
   const validValues: SquareValue[] = ["X", "O", "O"];
   const isWinner = isRowWinner(validValues);
