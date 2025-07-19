c3862fc27c3bef49d2452d1895102b7264d01e2e
Cat4848 <catalinrol@gmail.com>
2025-07-19 14:27:36 +0100

    test: extract diag from right to left

--- client/src/components/utils/isWinnerOnDiag/__tests__/isWinnerOnDiag.test.ts
+++ client/src/components/utils/isWinnerOnDiag/__tests__/isWinnerOnDiag.test.ts
@@ -1,7 +1,11 @@
-import { extractDiagLeftToRight } from "../isWinnerOnDiag";
+import {
+  extractDiagLeftToRight,
+  extractDiagRightToLeft
+} from "../isWinnerOnDiag";
 import { SquareValue } from "../../../../lib/types";
 
-test("if is extracts the diagonal correctly on a 3x3 board", () => {
+test("if is extracts the left to right diagonal correctly on a 3x3 board", () => {
+  expect.assertions(1);
   const board: SquareValue[][] = [
     ["O", "X", "O"],
     ["O", "O", "X"],
@@ -9,5 +13,17 @@ test("if is extracts the diagonal correctly on a 3x3 board", () => {
   ];
   const extractedDiag = extractDiagLeftToRight(board);
   const manualDiag = [board[0][0], board[1][1], board[2][2]];
+  expect(extractedDiag).toStrictEqual(manualDiag);
+});
+
+test("if is extracts the right to left diagonal correctly on a 3x3 board", () => {
+  expect.assertions(1);
+  const board: SquareValue[][] = [
+    ["O", "X", "O"],
+    ["O", "O", "X"],
+    ["X", "X", "O"]
+  ];
+  const manualDiag = [board[2][0], board[1][1], board[0][2]];
+  const extractedDiag = extractDiagRightToLeft(board);
   expect(extractedDiag).toStrictEqual(manualDiag);
 });
--- client/src/components/utils/isWinnerOnDiag/isWinnerOnDiag.ts
+++ client/src/components/utils/isWinnerOnDiag/isWinnerOnDiag.ts
@@ -6,3 +6,9 @@ export const extractDiagLeftToRight = (board: SquareValue[][]) => {
   const diag = board.map((row, i) => row[i]);
   return diag;
 };
+
+export const extractDiagRightToLeft = (board: SquareValue[][]) => {
+  const reversedBoard = board.reverse();
+  const diag = extractDiagLeftToRight(reversedBoard);
+  return diag;
+};
