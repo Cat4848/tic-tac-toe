34fcdb07a3306fba613eacea7c53255f554dd721
Cat4848 <catalinrol@gmail.com>
2025-07-19 13:45:38 +0100

    test: isWinner on a losing 5x5 board

--- client/src/components/utils/flipBoard/__tests__/flipBoard.test.ts
+++ client/src/components/utils/flipBoard/__tests__/flipBoard.test.ts
@@ -31,15 +31,31 @@ test("if it validates columns correctly on a winning 3X3 board", () => {
 test("if it validates columns correctly on a winning 5X5 board", () => {
   expect.assertions(1);
   const originalBoard: SquareValue[][] = [
-    ["O", "X", "X", 'X', 'X'] /**  */,
-    ["O", "X", "O", 'O', 'X'] /**  */,
-    ["X", "X", "X", 'O', 'X'] /** winning column */,
-    ["O", "O", "O", 'O', 'X'] /**  */,
-    ["X", "O", "O", 'O', 'X'] /**  */,
-    ["O", "X", "O", 'X', 'X'] /**  */,
+    ["O", "X", "X", "X", "X"] /**  */,
+    ["O", "X", "O", "O", "X"] /**  */,
+    ["X", "X", "X", "O", "X"] /** winning column */,
+    ["O", "O", "O", "O", "X"] /**  */,
+    ["X", "O", "O", "O", "X"] /**  */,
+    ["O", "X", "O", "X", "X"] /**  */
   ];
   const flippedBoard = flipBoard(originalBoard);
   const isWinner = isWinnerOnRows(flippedBoard);
 
   expect(isWinner).toBe(true);
+});
+
+test("if it validates columns correctly on a losing 5X5 board", () => {
+  expect.assertions(1);
+  const originalBoard: SquareValue[][] = [
+    ["O", "X", "X", "X", "X"],
+    ["O", "X", "O", "O", "O"],
+    ["X", "X", "X", "O", "X"],
+    ["O", "O", "O", "O", "X"],
+    ["X", "O", "O", "O", "X"],
+    ["O", "X", "O", "X", "X"]
+  ];
+  const flippedBoard = flipBoard(originalBoard);
+  const isWinner = isWinnerOnRows(flippedBoard);
+
+  expect(isWinner).toBe(false);
 });
