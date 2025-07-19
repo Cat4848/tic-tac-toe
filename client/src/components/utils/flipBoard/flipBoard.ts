9f82fe0dd403993cc1657ee7f492c1e4bf43e651
Cat4848 <catalinrol@gmail.com>
2025-07-19 15:12:37 +0100

    refactor: added deepCopy to flipBoard

--- client/src/components/utils/flipBoard/flipBoard.ts
+++ client/src/components/utils/flipBoard/flipBoard.ts
@@ -1,7 +1,8 @@
+import { deepCopy } from "..";
 import { SquareValue } from "../../../lib/types";
 
 const flipBoard = (board: SquareValue[][]) => {
-  const columns: SquareValue[][] = JSON.parse(JSON.stringify(board));
+  const columns = deepCopy(board);
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board.length; j++) {
       columns[i][j] = board[j][i];
