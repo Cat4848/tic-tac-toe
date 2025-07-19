590d345195fff7a883b11be7c8b5add90811f417
Cat4848 <catalinrol@gmail.com>
2025-07-19 10:24:29 +0100

    feat: built the X and O moves alternation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,19 +6,40 @@ export const Main = () => {
   const [board, setBoard] = useState<(XorO | undefined)[][]>([
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
-    ["O", undefined, undefined]
+    [undefined, undefined, undefined]
   ]);
+  const [moveNo, setMoveNo] = useState(0);
+  const isXNext = moveNo % 2 === 0;
 
   return (
     <div className="flex flex-col mt-10 items-center gap-10">
       <div className="font-bold text-2xl">Tic Tac Toe</div>
       <div className="flex flex-col gap-1">
-        {board.map((row) => (
+        {board.map((row, i) => (
           <div key={uuid()} className="flex gap-1">
-            {row.map((column) => (
+            {row.map((column, j) => (
               <div
                 key={uuid()}
                 className="border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex"
+                onClick={() =>
+                  setBoard((board) => {
+                    return board.map((column, k) => {
+                      return column.map((row, l) => {
+                        if (i === k && j === l && !row) {
+                          if (isXNext) {
+                            setMoveNo(moveNo + 1);
+                            return "X";
+                          } else {
+                            setMoveNo(moveNo + 1);
+                            return "O";
+                          }
+                        } else {
+                          return row;
+                        }
+                      });
+                    });
+                  })
+                }
               >
                 {column}
               </div>
