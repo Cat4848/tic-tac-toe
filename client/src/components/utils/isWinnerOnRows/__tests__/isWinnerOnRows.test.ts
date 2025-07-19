4294f38794b0b7cde047071775fd2c9c1ebb0228
Cat4848 <catalinrol@gmail.com>
2025-07-19 12:10:06 +0100

    test: validation on a 5X5 winning board

--- client/src/components/utils/isWinnerOnRows/__tests__/isWinnerOnRows.test.ts
+++ client/src/components/utils/isWinnerOnRows/__tests__/isWinnerOnRows.test.ts
@@ -1,12 +1,26 @@
 import { SquareValue } from "../../../../lib/types";
 import isWinnerOnRows from "../isWinnerOnRows";
 
-test("if it validates correctly a winning 3X3 board 1", () => {
+test("if it validates correctly a winning 3X3 board", () => {
   expect.assertions(1);
   const winningBoard: SquareValue[][] = [
     ["O", "O", "O"],
     ["O", "O", "X"],
     ["O", "X", "O"]
+  ];
+  const isWinner = isWinnerOnRows(winningBoard);
+  expect(isWinner).toBe(true);
+});
+
+test("if it validates correctly a winning 5X5 board", () => {
+  expect.assertions(1);
+  const winningBoard: SquareValue[][] = [
+    ["O", "O", "O", "X", "X"],
+    ["X", "X", "X", "X", "X"],
+    ["O", "O", "O", "X", "X"],
+    ["X", "O", "O", "X", "X"],
+    ["O", "X", "O", "X", "X"],
+    ["O", "O", "X", "X", "X"]
   ];
   const isWinner = isWinnerOnRows(winningBoard);
   expect(isWinner).toBe(true);
