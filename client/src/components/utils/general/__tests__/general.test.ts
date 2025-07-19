2059083145daffdfb67b9c6d9c589af2e1c24800
Cat4848 <catalinrol@gmail.com>
2025-07-19 15:34:12 +0100

    test: if the created board length is correct

--- client/src/components/utils/general/__tests__/general.test.ts
+++ client/src/components/utils/general/__tests__/general.test.ts
@@ -5,3 +5,9 @@ test("if the created board is an Array", () => {
   const board = buildBoard(size);
   expect(Array.isArray(board)).toBe(true);
 });
+
+test("if the created board length is correct", () => {
+  const size = 5;
+  const board = buildBoard(size);
+  expect(board.length).toBe(size);
+});
