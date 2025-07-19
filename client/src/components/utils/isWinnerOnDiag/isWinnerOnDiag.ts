a45461cde71ac95719a2662e2b09e909bf931083
Cat4848 <catalinrol@gmail.com>
2025-07-19 14:15:03 +0100

    refactor: more readable map function return value

--- client/src/components/utils/isWinnerOnDiag/isWinnerOnDiag.ts
+++ client/src/components/utils/isWinnerOnDiag/isWinnerOnDiag.ts
@@ -3,8 +3,6 @@ import { SquareValue } from "../../../lib/types";
 const isWinnerOnDiag = () => {};
 
 export const extractDiagLeftToRight = (board: SquareValue[][]) => {
-  const diag = board.map((row, i) => {
-    return row[i];
-  });
+  const diag = board.map((row, i) => row[i]);
   return diag;
 };
