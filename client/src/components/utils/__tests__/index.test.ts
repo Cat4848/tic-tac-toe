70ab2367cdcc4e4d3cd9d308131c22bc9ef2b454
Cat4848 <catalinrol@gmail.com>
2025-07-19 11:39:54 +0100

    refactor: tests for row validation

--- client/src/components/utils/__tests__/index.test.ts
+++ client/src/components/utils/__tests__/index.test.ts
@@ -1,19 +1,22 @@
 import { isRowWinner } from "..";
 import { SquareValue } from "../../../lib/types";
 
-test("if it validates correctly a winning row", () => {
+test("if it validates correctly a winning O row", () => {
+  expect.assertions(1);
   const validValues: SquareValue[] = ["O", "O", "O"];
   const isWinner = isRowWinner(validValues);
   expect(isWinner).toBe(true);
 });
 
-test("if it validates correctly a winning row", () => {
+test("if it validates correctly a winning X row", () => {
+  expect.assertions(1);
   const validValues: SquareValue[] = ["X", "X", "X", "X"];
   const isWinner = isRowWinner(validValues);
   expect(isWinner).toBe(true);
 });
 
 test("if it validates correctly a losing row", () => {
+  expect.assertions(1);
   const validValues: SquareValue[] = ["X", "O", "O"];
   const isWinner = isRowWinner(validValues);
   expect(isWinner).toBe(false);
