74ff3f42729a9094476ef4b1ebe73c429d0b9b80
Cat4848 <catalinrol@gmail.com>
2025-07-19 18:09:30 +0100

    refactor: removed unnecessary logs

--- server/src/server.ts
+++ server/src/server.ts
@@ -48,8 +48,4 @@ app.get("*", async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () =>
-  console.log(
-    `server listening on PORT: ${PORT} and env: ${process.env.NODE_ENV}`
-  )
-);
+app.listen(PORT, () => console.log(`server listening on PORT: ${PORT}`));
