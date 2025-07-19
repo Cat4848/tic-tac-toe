aa1bc034971d29dbe9126d42c4e3224ed8f8c337
Cat4848 <catalinrol@gmail.com>
2025-07-19 20:24:18 +0100

    test: if it fails with wrong player_id prop data type

--- server/src/lib/validators/validateEditScore/__tests__/validateEditScore.test.ts
+++ server/src/lib/validators/validateEditScore/__tests__/validateEditScore.test.ts
@@ -42,3 +42,13 @@ test("if it fails with missing player_id prop", async () => {
     expect(e).toBeInstanceOf(ValidationError);
   }
 });
+
+test("if it fails with wrong player_id prop type", async () => {
+  expect.assertions(1);
+  const reqBody = { player_id: new Set(), score: 1 };
+  try {
+    await editScorePayloadValidation(reqBody);
+  } catch (e) {
+    expect(e).toBeInstanceOf(ValidationError);
+  }
+});
