8f094c53a32c4aa8746c27bd6078f63b190dfd6b
Cat4848 <catalinrol@gmail.com>
2025-07-19 19:17:17 +0100

    feat: built the editScore method

--- server/src/services/PlayersTable/PlayersTable.ts
+++ server/src/services/PlayersTable/PlayersTable.ts
@@ -1,4 +1,4 @@
-import { Database } from "../../lib/types";
+import { Database, Player } from "../../lib/types";
 
 export default class PlayersTable {
   private db: Database;
@@ -10,6 +10,12 @@ export default class PlayersTable {
   getAll = async () => {
     const sql = "SELECT * FROM players;";
     const result = await this.db.execute(sql);
+    return result;
+  };
+
+  editScore = async (player: Player, score: number) => {
+    const sql = `UPDATE players SET score = ? WHERE player_id = ?;`;
+    const result = await this.db.execute(sql, [score, player.player_id]);
     return result;
   };
 }
