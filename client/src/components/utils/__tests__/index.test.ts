9b29be33c8194206afa88879954399cb98b21ae7
Cat4848 <catalinrol@gmail.com>
2025-07-19 11:37:50 +0100

    test: losing row validation

--- client/src/components/utils/__tests__/index.test.ts
+++ client/src/components/utils/__tests__/index.test.ts
@@ -6,3 +6,9 @@ test("if it validates correctly a winning row", () => {
   const isWinner = isRowWinner(validValues);
   expect(isWinner).toBe(true);
 });
+
+test("if it validates correctly a losing row", () => {
+  const validValues: SquareValue[] = ["X", "O", "O"];
+  const isWinner = isRowWinner(validValues);
+  expect(isWinner).toBe(false);
+});
